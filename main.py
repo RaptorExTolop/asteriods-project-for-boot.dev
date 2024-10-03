13a2b2548adb42d0b89a0ec54e17086f5d52442b
RaptorExTolop <jamesappletonret@gmail.com>
2024-10-03 21:26:40 +0930

    6: added player class and made it so the character could turn left and right

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    pygame.init
+    pygame.init()
+
+    game_time = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print("Quiting!")
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = game_time.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
